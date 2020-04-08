a44cba89c1b88851a801accd17c273777c5297ce
thegtzz <thegtzz@gmail.com>
2020-04-08 18:55:27 +0300

    Take out state to separate file + passing it via props in index.js for App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
@@ -8,23 +8,24 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import Friends from "./components/Friends/Friends";
 
 
-const App = () => {
+const App = (props) => {
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
-                <Navbar/>
+                <Navbar state={props.state.sidebar}/>
                 <div className='app-wrapper-content'>
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' render={() => <Profile
+                        state={props.state.profilePage}/>}/>
+                    <Route path='/dialogs' render={() => <Dialogs
+                        state={props.state.dialogsPage} />}/>
                     <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
---
+++ src/redux/State.js
@@ -0,0 +1,37 @@
+let state = {
+    profilePage: {
+        posts: [
+    {id: 1, message: 'Hi, how are you?', likesCount: 12},
+    {id: 2, message: 'It\'s my first post.', likesCount: 11},
+    {id: 3, message: 'It\'s my second post.', likesCount: 11},
+    {id: 4, message: 'Go on rolling.', likesCount: 11},
+]
+    },
+    dialogsPage: {
+        messages: [
+            {id: 1, message: 'HI'},
+            {id: 2, message: 'How s your react?'},
+            {id: 3, message: 'Whats up?'},
+            {id: 4, message: 'Its ok'},
+            {id: 5, message: 'Go code in react?'},
+        ],
+        dialogs: [
+            {id: 1, name: 'Yan'},
+            {id: 2, name: 'Lera'},
+            {id: 3, name: 'Alina'},
+            {id: 4, name: 'Dimon'},
+            {id: 5, name: 'Vlad'},
+        ]
+    },
+    sidebar: {
+        friends: [
+            {id: 1, name: 'Yan'},
+            {id: 2, name: 'Lera'},
+            {id: 3, name: 'Alina'},
+            {id: 4, name: 'Dimon'},
+            {id: 5, name: 'Vlad'},
+        ]
+    }
+}
+
+export default state
