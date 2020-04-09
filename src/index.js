10897c22b59320b2f595ae48c12941887b2d973e
thegtzz <thegtzz@gmail.com>
2020-04-09 17:27:10 +0300

    Escaping from circle-dependency between index.js and state.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state from "./redux/State";
+import React from "react";
+import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import store from "./redux/State";
 
-ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App state={state}/>
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+
+rerenderEntireTree(store.getState())
+
+store.subscribe(rerenderEntireTree)
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
