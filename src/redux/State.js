6d8ae43d18511b48af27bcca0939c56765f72261
thegtzz <thegtzz@gmail.com>
2020-04-09 17:25:31 +0300

    Creating self-store (similiar to Redux-store)

--- src/redux/State.js
+++ src/redux/State.js
@@ -1,37 +1,65 @@
-let state = {
-    profilePage: {
-        posts: [
-    {id: 1, message: 'Hi, how are you?', likesCount: 12},
-    {id: 2, message: 'It\'s my first post.', likesCount: 11},
-    {id: 3, message: 'It\'s my second post.', likesCount: 11},
-    {id: 4, message: 'Go on rolling.', likesCount: 11},
-]
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post.', likesCount: 11},
+                {id: 3, message: 'It\'s my second post.', likesCount: 11},
+                {id: 4, message: 'Go on rolling.', likesCount: 11},
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'HI'},
+                {id: 2, message: 'How s your react?'},
+                {id: 3, message: 'Whats up?'},
+                {id: 4, message: 'Its ok'},
+                {id: 5, message: 'Go code in react?'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Yan'},
+                {id: 2, name: 'Lera'},
+                {id: 3, name: 'Alina'},
+                {id: 4, name: 'Dimon'},
+                {id: 5, name: 'Vlad'},
+            ]
+        },
+        sidebar: {
+            friends: [
+                {id: 1, name: 'Yan'},
+                {id: 2, name: 'Lera'},
+                {id: 3, name: 'Alina'},
+                {id: 4, name: 'Dimon'},
+                {id: 5, name: 'Vlad'},
+            ]
+        }
     },
-    dialogsPage: {
-        messages: [
-            {id: 1, message: 'HI'},
-            {id: 2, message: 'How s your react?'},
-            {id: 3, message: 'Whats up?'},
-            {id: 4, message: 'Its ok'},
-            {id: 5, message: 'Go code in react?'},
-        ],
-        dialogs: [
-            {id: 1, name: 'Yan'},
-            {id: 2, name: 'Lera'},
-            {id: 3, name: 'Alina'},
-            {id: 4, name: 'Dimon'},
-            {id: 5, name: 'Vlad'},
-        ]
+    getState() {
+        debugger
+        return this._state
     },
-    sidebar: {
-        friends: [
-            {id: 1, name: 'Yan'},
-            {id: 2, name: 'Lera'},
-            {id: 3, name: 'Alina'},
-            {id: 4, name: 'Dimon'},
-            {id: 5, name: 'Vlad'},
-        ]
-    }
+    _callSubscriber() {
+        console.log('State is changed')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    },
 }
 
-export default state
+export default store
+window.store = store
