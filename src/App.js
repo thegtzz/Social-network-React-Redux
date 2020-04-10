41b756f92cd279bad02a3bc59046e55880f7a3e6
thegtzz <thegtzz@gmail.com>
2020-04-10 15:12:44 +0300

    Refactor for action-dispatching

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ const App = (props) => {
                 <div className='app-wrapper-content'>
                     <Route path='/profile' render={() => <Profile
                         profilePage={props.state.profilePage}
-                            addPost={props.addPost}
-                        updateNewPostText={props.updateNewPostText}/>}/>
+                           dispatch={props.dispatch}/>}/>
                     <Route path='/dialogs' render={() => <Dialogs
-                        state={props.state.dialogsPage} />}/>
+                        state={props.state.dialogsPage}
+                        dispath={props.dispatch}/>}/>
                     <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,20 +1,21 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/State";
 
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-    let newPostElement = React.createRef()
 
     let addPost = () => {
-        props.addPost()
+        props.dispatch(addPostActionCreator())
     }
 
-    let newPostChange = () => {
-        let text = newPostElement.current.value
-        props.updateNewPostText(text)
+    let newPostChange = (e) => {
+        let text = e.target.value
+        let action = updateNewPostTextActionCreator(text)
+        props.dispatch(action)
     }
 
     return (
@@ -22,7 +23,7 @@ const MyPosts = (props) => {
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea onChange={newPostChange} ref={newPostElement} value={props.newPostText}/>
+                    <textarea onChange={newPostChange} value={props.newPostText}/>
                 </div>
                 <div>
                     <button onClick={ addPost }>Add post</button>
