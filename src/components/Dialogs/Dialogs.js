c57a609f3327d1e5454ab2f468ab7881c535dd06
thegtzz <thegtzz@gmail.com>
2020-04-10 15:11:32 +0300

    Dispatch method for messages

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/State";
 
 
 const Dialogs = (props) => {
@@ -9,10 +10,15 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id}/> )
     let messagesElements = props.state.messages.map( m => <Message message={m.message}/> )
 
-    let newMessageElement = React.createRef()
+
     let sendMessage = () => {
-        let text = newMessageElement.current.value
-        alert(text)
+        props.dispath(addMessageActionCreator())
+    }
+
+    let newMessageChange = (e) => {
+        let text = e.target.value
+        let action = updateNewMessageTextActionCreator(text)
+        props.dispath(action)
     }
 
     return (
@@ -23,7 +29,7 @@ const Dialogs = (props) => {
             <div className={s.messages}>
                 { messagesElements }
                 <div>
-                    <textarea ref={newMessageElement}></textarea>
+                    <textarea onChange={ newMessageChange } value={props.state.newMessageText}/>
                 </div>
                 <div>
                     <button onClick={ sendMessage }>Send Message</button>
