568248e9262ec61125cc617343a0bd4cdc126fe3
thegtzz <thegtzz@gmail.com>
2020-04-13 18:08:31 +0300

    Added section for Friend-list

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,15 +2,18 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import friendsReducer from "./friends-reducer";
 
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    sidebar: sidebarReducer
+    sidebar: sidebarReducer,
+    friendsPage: friendsReducer
 })
 
 let store = createStore(reducers)
 
+window.store = store
 
 export default store
