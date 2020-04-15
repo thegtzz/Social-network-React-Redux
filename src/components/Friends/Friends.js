2420c38d29abb4d2e825c53322cee83d0871349e
thegtzz <thegtzz@gmail.com>
2020-04-15 13:02:38 +0300

    API get-request for users using axios

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,42 +1,24 @@
 import React from "react";
 import s from './Friends.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 const Friends = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLjTsWnfiq5ME7swO8un1LUlz92NWwHgeVm4MGZa-vMc3cT7HI&usqp=CAU',
-                followed: true,
-                fullname: 'Yan',
-                status: 'I\'m a boss',
-                location: {city: 'Warsaw', country: 'Poland'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLjTsWnfiq5ME7swO8un1LUlz92NWwHgeVm4MGZa-vMc3cT7HI&usqp=CAU',
-                followed: false,
-                fullname: 'Lera',
-                status: 'I\'m a bank-manager',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLjTsWnfiq5ME7swO8un1LUlz92NWwHgeVm4MGZa-vMc3cT7HI&usqp=CAU',
-                followed: true,
-                fullname: 'Dimon',
-                status: 'I\'m a dota-gamer',
-                location: {city: 'Mogilev', country: 'Belarus'}
-            },
-        ])
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div className={s.friend_row}>
+            <button onClick={ getUsers }>Get users</button>
             {props.users.map(u => <div key={u.id}>
                 <span>
-                <div>
-                    <img src={u.photoUrl} alt=""/>
+                <div className={s.friends}>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
                 </div>
                 <div>
                     {u.followed
@@ -46,12 +28,12 @@ const Friends = (props) => {
             </span>
                 <span>
                 <span>
-                    <div>{u.fullname}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
             </div>)}
