62004a127878cc88d8d9a6e18a3c8ba7d4c4334a
thegtzz <thegtzz@gmail.com>
2020-04-16 21:03:30 +0300

    Slight changes with routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {Route} from "react-router-dom";
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
     return (
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer/>
                 <Navbar />
                 <div className='app-wrapper-content'>
-                    <Route path='/profile' render={() => <Profile/>}/>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
                     <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                     <Route path='/friends' render={() => <FriendsContainer/>}/>
                     <Route path='/news' component={News}/>
