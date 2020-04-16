2ecf0ec11c34204d5b97dd87a14730406ea61a76
thegtzz <thegtzz@gmail.com>
2020-04-16 21:04:25 +0300

    Making API POST request to follow/unfollow users

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Friends.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 
 let Friends = (props) => {
@@ -29,12 +30,35 @@ let Friends = (props) => {
                     </NavLink>
                 </div>
                 <div>
+                    {/*<button onClick={ (e) => props.onClicked(u.id) }>Follow</button>*/}
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                             axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                withCredentials: true,
+                                 headers: {
+                                    "API-KEY": '026e3091-bf2e-480b-a762-84e0bb53c807'
+                                 }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.unfollow(u.id)
+                                    }
+                                })
+
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                 headers: {
+                                    "API-KEY": '026e3091-bf2e-480b-a762-84e0bb53c807'
+                                 }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.follow(u.id)
+                                    }
+                                })
+
                         }}>Follow</button>}
                 </div>
                 </span>
--- src/components/Friends/FriendsContainer.js
+++ src/components/Friends/FriendsContainer.js
@@ -11,13 +11,17 @@ import {
 import * as axios from "axios";
 import Friends from "./Friends";
 import Preloader from "../common/Preloader/Preloader";
+import {withRouter} from "react-router-dom";
 
 
 class FriendsContainer extends React.Component {
     componentDidMount() {
         if (this.props.users.length === 0) {
             this.props.toggleIsFetching(true)
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+                {
+                    withCredentials: true
+                })
                 .then(response => {
                     this.props.toggleIsFetching(false)
                     this.props.setUsers(response.data.items)
@@ -29,7 +33,10 @@ class FriendsContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true)
-         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+             {
+                 withCredentials: true
+             })
                 .then(response => {
                     this.props.toggleIsFetching(false)
                     this.props.setUsers(response.data.items)
