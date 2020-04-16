146e7234456711d736a9908b324139724ec89ea2
thegtzz <thegtzz@gmail.com>
2020-04-16 21:02:41 +0300

    Created HeaderContainer, refactor + styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
                 <img src="https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png" alt=""/>
-           </header>
+           <div className={s.loginBlock}>
+               {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+           </div>
+            </header>
 }
 
 export default Header
