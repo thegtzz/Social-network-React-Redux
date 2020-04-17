a41915c5690a9cb26ea1ab4aa98b75d3e0c41ef3
thegtzz <thegtzz@gmail.com>
2020-04-17 19:40:43 +0300

    thunkMiddleware is added to redux-store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import friendsReducer from "./friends-reducer";
 import authReducer from './auth-reducer'
+import thunkMiddleWare from 'redux-thunk'
 
 
 let reducers = combineReducers({
@@ -14,7 +15,7 @@ let reducers = combineReducers({
     auth: authReducer,
 })
 
-let store = createStore(reducers)
+let store = createStore(reducers, applyMiddleware(thunkMiddleWare))
 
 window.store = store
 
