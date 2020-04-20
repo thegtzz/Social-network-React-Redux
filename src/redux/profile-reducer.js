7cd13071d6909bc84711700e80cdf4878158badb
thegtzz <thegtzz@gmail.com>
2020-04-20 18:09:47 +0300

    Thunk funcs for getting and update status

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI} from "../api/api";
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
+const SET_STATUS = 'SET_STATUS'
 
 let initialState = {
             posts: [
@@ -13,6 +14,7 @@ let initialState = {
             ],
             newPostText: '',
             profile: null,
+            status: ''
         }
 
 const profileReducer = (state = initialState, action) => {
@@ -26,6 +28,9 @@ const profileReducer = (state = initialState, action) => {
         }
         case UPDATE_NEW_POST_TEXT:
             return {...state, newPostText: action.newText}
+
+        case SET_STATUS:
+            return {...state, status: action.status}
 
         case SET_USER_PROFILE:
             return {...state, profile: action.profile}
@@ -50,12 +55,34 @@ export const updateNewPostTextActionCreator = (text) => {
 
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile})
 
+export const setStatus = (status) => ({ type: SET_STATUS, status})
+
 
 export const getUserProfile = (userId) => {
     return (dispatch) => {
         profileAPI.getUserProfile(userId)
             .then(data => {
                 dispatch(setUserProfile(data))
+            })
+    }
+}
+
+export const getStatus = (userId) => {
+    return (dispatch) => {
+        profileAPI.getStatus(userId)
+            .then(response => {
+                dispatch(setStatus(response.data))
+            })
+    }
+}
+
+export const updateStatus = (status) => {
+    return (dispatch) => {
+        profileAPI.updateStatus(status)
+            .then(response => {
+                if(response.data.resultCode === 0) {
+                    dispatch(setStatus(status))
+                }
             })
     }
 }
