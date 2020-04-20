e050b751a07f430c528897ad989e3f98afbc2004
thegtzz <thegtzz@gmail.com>
2020-04-20 11:53:44 +0300

    Profile thunk added

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,5 @@
+import {profileAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
@@ -47,5 +49,15 @@ export const updateNewPostTextActionCreator = (text) => {
 }
 
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile})
+
+
+export const getUserProfile = (userId) => {
+    return (dispatch) => {
+        profileAPI.getUserProfile(userId)
+            .then(data => {
+                dispatch(setUserProfile(data))
+            })
+    }
+}
 
 export default profileReducer
