be8d520ef89a5554411ae5d49ef0a186c1457975
thegtzz <thegtzz@gmail.com>
2020-04-21 18:54:57 +0300

    login/logout buttons now works

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,9 @@ const Header = (props) => {
     return <header className={s.header}>
                 <img src="https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png" alt=""/>
            <div className={s.loginBlock}>
-               {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+               {props.isAuth
+                   ? <div>{props.login} - <button onClick={props.logOut}>Log out</button></div>
+                   : <NavLink to={'/login'}>Login</NavLink>}
            </div>
             </header>
 }
