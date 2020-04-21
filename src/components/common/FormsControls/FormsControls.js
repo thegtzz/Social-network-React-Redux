4524f035296ecb687e974b8f98901a732118a766
thegtzz <thegtzz@gmail.com>
2020-04-21 18:52:40 +0300

    Created Form control (HOC file) for displaying input-field errors + styling

---
+++ src/components/common/FormsControls/FormsControls.js
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "./FormsControls.module.css"
+
+
+export const Element = Element => ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={s.formControl + " " + (hasError ? s.error : "")}>
+            <div>
+                <Element {...input} {...props} />
+            </div>
+            { hasError && <span>{meta.error}</span>}
+        </div>
+    )
+}
