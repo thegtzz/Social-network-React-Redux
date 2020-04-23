42ceccf7ce756c7f8fe0c017f5adc664573e6e27
thegtzz <thegtzz@gmail.com>
2020-04-23 15:34:29 +0300

    Delete method for posts

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI} from "../api/api";
 const ADD_POST = 'ADD-POST'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET_STATUS'
+const DELETE_POST = 'DELETE_POST'
 
 let initialState = {
             posts: [
@@ -32,6 +33,8 @@ const profileReducer = (state = initialState, action) => {
         case SET_USER_PROFILE:
             return {...state, profile: action.profile}
 
+        case DELETE_POST:
+            return {...state, posts: state.posts.filter(p => p.id != action.postId)}
         default:
             return state
     }
@@ -40,9 +43,12 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = (text) => {
     return {
         type: ADD_POST,
-        PostText: text.PostMessage
+        PostText: text
     }
 }
+
+export const deletePost = (postId) => ({type: DELETE_POST, postId})
+
 
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile})
 
