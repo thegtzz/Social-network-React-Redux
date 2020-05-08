d7a429ecb968eb2369febc72a9db32aa2510b111
thegtzz <thegtzz@gmail.com>
2020-05-08 13:17:23 +0300

    Style-finished Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
 import s from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import logoImage from '../../assets/images/logo.jpg'
+import {LoginElement} from "./LoginElement/LoginElement";
 
 
 const Header = (props) => {
     return <header className={s.header}>
-                <img src="https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png" alt=""/>
-           <div className={s.loginBlock}>
-               {props.isAuth
-                   ? <div>{props.login} - <button onClick={props.logOut}>Log out</button></div>
-                   : <NavLink to={'/login'}>Login</NavLink>}
-           </div>
-            </header>
+        <NavLink to={'/news'}>
+            <img src={logoImage} alt=""/>
+            <span>#stayhome</span>
+        </NavLink>
+
+        {props.isAuth
+            ? <LoginElement {...props}/>
+            : <div className={s.loginBlock}><NavLink to={'/login'}>Login</NavLink></div>
+        }
+
+    </header>
 }
 
 export default Header
--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -5,14 +5,17 @@ import {logOut} from "../../redux/auth-reducer";
 
 
 class HeaderContainer extends React.Component {
+
     render() {
-        return <Header {...this.props}/>
+        return <Header {...this.props} profile={this.props.profile}/>
     }
 }
 
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
+    profile: state.profilePage,
+    authorizedUserId: state.auth.userId,
 })
 
 export default connect(mapStateToProps, {logOut})(HeaderContainer)
