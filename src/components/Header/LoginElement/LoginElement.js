7a85e3c7c033669bf7a7a9196f36e33bcd83be57
thegtzz <thegtzz@gmail.com>
2020-05-08 13:12:59 +0300

    top_profile_menu component

---
+++ src/components/Header/LoginElement/LoginElement.js
@@ -0,0 +1,40 @@
+import React from "react";
+import s from "../Header.module.css";
+import {NavLink} from "react-router-dom";
+import userImage from '../../../assets/images/user.png'
+import {useOutsideClick} from "../CustomHeaderHooks/useOutsideClick";
+import Preloader from "../../common/Preloader/Preloader";
+
+
+export const LoginElement = (props) => {
+    const {showMenu, setShowMenu, ref} = useOutsideClick(false)
+
+    const handleClick = () => {
+        setShowMenu(!showMenu)
+    }
+
+    if (!props.profile.profile) {
+        return <Preloader/>
+    }
+
+    return (
+        <div ref={ref} className={s.loginBlock} onClick={handleClick}>
+            <a>
+                <div className={s.userName}>{props.login}</div>
+                <img src={props.profile.profile.photos.small || userImage} alt=''/>
+                <div className={s.loginBlockArrow}></div>
+            </a>
+            {showMenu
+                && <div className={s.topDroppedMenu}>
+                    <NavLink to={'/profile'}>My profile</NavLink>
+                    <div className={s.topDivSep}></div>
+                    <NavLink to={'/settings'}>Settings</NavLink>
+                    <NavLink to={'/help'}>Help</NavLink>
+                    <div className={s.topDivSep}></div>
+                    <div onClick={props.logOut}>
+                        <NavLink to={'/login'}>Log out</NavLink>
+                    </div>
+                  </div>}
+        </div>
+    )
+}
