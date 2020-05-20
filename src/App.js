cd7d51086f8b2150b42ed89e9bb2cea797535c70
thegtzz <thegtzz@gmail.com>
2020-05-20 16:56:46 +0300

    Rebuild App.js from class-based to functional component + fixed export default with named export

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
@@ -10,43 +10,41 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import Login from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
-import Preloader from "./components/common/Preloader/Preloader";
+import {Preloader} from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
 
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
 const FriendsContainer = React.lazy(() => import("./components/Friends/FriendsContainer"))
 
-class App extends React.Component {
-    componentDidMount() {
-        this.props.initializeApp()
+const App = props => {
+    useEffect(() => {
+        props.initializeApp()
+    })
+
+    if(!props.initialized) {
+        return <Preloader/>
     }
 
-    render() {
-        if(!this.props.initialized) {
-            return <Preloader/>
-        }
-
-        return (
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Redirect from={"/"} to={"/profile"}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <React.Suspense fallback={<Preloader/>}>
-                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    </React.Suspense>
-                    <React.Suspense fallback={<Preloader/>}>
-                        <Route path='/friends' render={() => <FriendsContainer/>}/>
-                    </React.Suspense>
-                    <Route path='/login' render={() => <Login/>}/>
-                    <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/settings' component={Settings}/>
-                </div>
+    return (
+        <div className='app-wrapper'>
+            <HeaderContainer/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Redirect from={"/"} to={"/profile"}/>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                <React.Suspense fallback={<Preloader/>}>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                </React.Suspense>
+                <React.Suspense fallback={<Preloader/>}>
+                    <Route path='/friends' render={() => <FriendsContainer/>}/>
+                </React.Suspense>
+                <Route path='/login' render={() => <Login/>}/>
+                <Route path='/news' component={News}/>
+                <Route path='/music' component={Music}/>
+                <Route path='/settings' component={Settings}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
@@ -56,12 +54,10 @@ const mapStateToProps = (state) => ({
 const AppContainer = connect(mapStateToProps, {initializeApp})(App);
 
 
-const MainApp = (props) => {
+export const MainApp = (props) => {
     return <BrowserRouter>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
     </BrowserRouter>
 }
-
-export default MainApp
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as serviceWorker from './serviceWorker';
 import React from "react";
 import ReactDOM from "react-dom";
-import MainApp from "./App";
+import {MainApp} from "./App";
 
 
 ReactDOM.render(
