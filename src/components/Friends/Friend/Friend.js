9f834cc356b820e705013819b52bd4b836b59065
thegtzz <thegtzz@gmail.com>
2020-05-29 17:03:36 +0300

    Friend section styled

--- src/components/Friends/Friend/Friend.js
+++ src/components/Friends/Friend/Friend.js
@@ -1,43 +1,32 @@
 import React from "react";
-import s from "../Friends.module.css";
+import s from "./Friend.module.css";
 import {NavLink} from "react-router-dom";
-import userPhoto from "../../../assets/images/user.png";
+import userPhoto from "../../../assets/images/upload_profile_photo.png";
 
 
 const Friend = (props) => {
     let u = props.user
     return (
-        <div key={u.id}>
-                <span>
-                <div className={s.friends}>
-                    <NavLink to={'/profile/' + u.id}>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
-                    </NavLink>
-                </div>
-                <div>
-                    {u.followed
-                        ? <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                  onClick={() => {
-                                      props.unfollow(u.id)
-                                  }}>
-                            Unfollow</button>
-                        : <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                  onClick={() => {
-                                      props.follow(u.id)
-                                  }}>
-                            Follow</button>}
-                </div>
-                </span>
-            <span>
-                    <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{"u.location.country"}</div>
-                        <div>{"u.location.city"}</div>
-                    </span>
-                </span>
+        <div key={u.id} className={s.friendItem}>
+            <NavLink to={'/profile/' + u.id} className={s.navImg}>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
+            </NavLink>
+            <div className={s.fullname}>{u.name}</div>
+            <div className={s.followUser}>
+                {u.followed
+                    ? <button disabled={props.followingInProgress.some(id => id === u.id)}
+                              onClick={() => {
+                                  props.unfollow(u.id)
+                              }}
+                              className={s.btnFollow}>
+                        Unfollow</button>
+                    : <button disabled={props.followingInProgress.some(id => id === u.id)}
+                              onClick={() => {
+                                  props.follow(u.id)
+                              }}
+                              className={s.btnFollow}>
+                        Follow</button>}
+            </div>
         </div>
     )
 }
--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,20 +1,24 @@
 import React from "react";
 import s from "./Friends.module.css";
-import Paginator from "../common/Paginator/Paginator";
+import {Paginator} from "../common/Paginator/Paginator";
 import Friend from "./Friend/Friend";
 
 
 let Friends = ({totalUsersCount, currentPage, onPageChanged, pageSize, ...props}) => {
     return (
-        <div className={s.friend_row}>
-            <Paginator totalUsersCount={totalUsersCount} currentPage={currentPage} onPageChanged={onPageChanged} pageSize={pageSize}/>
-
+        <div className={s.friendList}>
+            <div className={s.friendCount}>
+                All friends <span className={s.totalUsers}>{totalUsersCount}</span>
+            </div>
             {props.users.map(u => <Friend user={u}
                                           follow={props.follow}
                                           unfollow={props.unfollow}
                                           key={u.id}
                                           followingInProgress={props.followingInProgress}/>
             )}
+            <div className={s.paginator}>
+                <Paginator totalUsersCount={totalUsersCount} currentPage={currentPage} onPageChanged={onPageChanged} pageSize={pageSize}/>
+            </div>
         </div>
     )
 }
