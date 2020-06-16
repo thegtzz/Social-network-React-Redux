9e766d02d470935b718216acd8dce3c41d689ba4
thegtzz <thegtzz@gmail.com>
2020-06-16 17:48:40 +0300

    default to named export

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import News from "./components/News/News";
+import {News} from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import HeaderContainer from "./components/Header/HeaderContainer";
