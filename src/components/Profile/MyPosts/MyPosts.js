f600df665ac393cc512b6f4dde06d88ec1c714d5
thegtzz <thegtzz@gmail.com>
2020-07-02 13:01:03 +0300

    Creating auto-sizing textarea function

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {Form, Field} from "react-final-form";
 import s from './MyPosts.module.css'
 import pm from '../Profile.module.css'
 import cn from 'classnames'
-import {Post} from "./Post/Post";
-import {Form, Field} from "react-final-form";
-import {maxLengthCreator} from "../../../utils/validators/validators";
-import {Element} from "../../common/FormsControls/FormsControls";
 import userImage from "../../../assets/images/upload_profile_photo.png";
+import {Element} from "../../common/FormsControls/FormsControls";
+import {maxLengthCreator} from "../../../utils/validators/validators";
+import {Post} from "./Post/Post";
 import {ProfileFriendsBlock} from "../../Friends/ProfileFriendsBlock/ProfileFriendsBlock";
 
 
 const Textarea = Element('textarea')
-const maxLength280 = maxLengthCreator(280)
+const maxLength2000 = maxLengthCreator(2000)
 
 const MyPostsForm = ({profile, onSubmit}) => {
+    useEffect(() => {
+        let textarea = document.querySelector('textarea')
+        textarea.addEventListener('input', function () {
+            this.style.height = 'auto'
+            this.style.height = this.scrollHeight + 'px'
+        })
+        textarea.style.height = 'auto'
+    })
+
     return (
         <Form onSubmit={onSubmit}>
             {({handleSubmit, form}) => (
@@ -22,11 +31,12 @@ const MyPostsForm = ({profile, onSubmit}) => {
                     form.reset()}
                 } className={cn(pm.sendPost, s.postsBlock)}>
                     <div className={s.postField}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                         <a>
                             <img src={profile.photos.small || userImage} alt=''/>
                         </a>
                         <Field placeholder={"Write something..."} name={"PostMessage"}
-                               component={Textarea} validate={maxLength280}/>
+                               component={Textarea}  validate={maxLength2000}/>
                     </div>
                     <div className={s.submitPost}>
                         <div className={s.addPostButtonWrap}>
@@ -40,7 +50,6 @@ const MyPostsForm = ({profile, onSubmit}) => {
 }
 
 export const MyPosts = (props) => {
-
     let postsElements = props.posts.map( p => <Post message={p.message}
                                                     profile={props.profile}
                                                     key={p.id}/>)
