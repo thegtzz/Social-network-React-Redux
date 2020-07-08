fd7c7fd37ded545eb8a6b416169efb72cee75739
thegtzz <thegtzz@gmail.com>
2020-07-08 13:11:23 +0300

    General types for reducers

---
+++ src/types/types.ts
@@ -0,0 +1,32 @@
+export type PostType = {
+    id: number
+    message: string
+    likesCount: number
+}
+export type ContactsType = {
+    github: string
+    vk: string
+    facebook: string
+    twitter: string
+    website: string
+    youtube: string
+    mainLink: string
+}
+export type PhotosType = {
+    small: string | null
+    large: string | null
+}
+export type ProfileType = {
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullname: string
+    contacts: ContactsType
+    photos: PhotosType
+}
+export type UserType = {
+    id: number
+    name: string
+    status: string
+    photos: PhotosType
+}
