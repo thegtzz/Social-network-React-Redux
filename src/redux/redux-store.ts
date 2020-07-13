adebec1cf26c8c67420e75d3823ecac8f8fe6940
thegtzz <thegtzz@gmail.com>
2020-07-13 16:14:10 +0300

    Created general thunktype for thunks

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {Action, applyMiddleware, combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import friendsReducer from "./friends-reducer";
 import authReducer from './auth-reducer'
-import thunkMiddleWare from 'redux-thunk'
+import thunkMiddleWare, {ThunkAction} from 'redux-thunk'
 // import {reducer as formReducer} from 'redux-form'
 import appReducer from "./app-reducer";
 import {newsCommentReducer} from "./newsComment-reducer";
@@ -23,6 +23,10 @@ let rootReducer = combineReducers({
 
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
+
+type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
+export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
+export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleWare))
 
