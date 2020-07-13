e451b50aed385f047810cf970f983d3cf913b75e
thegtzz <thegtzz@gmail.com>
2020-07-13 16:16:47 +0300

    Tests typed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import MainApp from './App';
+import {MainApp} from './App';
 import {act} from "@testing-library/react";
 
 it('renders without crashing', async () => {
